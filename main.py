06867f66d9c6d889174fd0c2aa7bd25285e8cbb2
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2020-12-04 01:17:58 +0900

    write todo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 
 import requests
 
+# TODO: 公式かviewerかそれともchunirecか使う方を決める
 music_data = requests.get("https://chuniviewer.net/GetMusicData.php").json()
 print(music_data["150"]["name"])
 # with open("music_list.json", "w", encoding="utf-8_sig") as f:
