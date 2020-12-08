da37686d09feb0d7fca25d8e828255a6cb80cda5
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2020-12-09 01:20:38 +0900

    仮挿入

--- crsbot_source/random_select.py
+++ crsbot_source/random_select.py
@@ -2,4 +2,4 @@ import random
 import json
 
 def random_select(music_number=3, difficulty=None, dif_range=None, category=None, artist=None, notes=None, notes_range=None, bpm=None, bpm_range=None):
-    
+    pass
