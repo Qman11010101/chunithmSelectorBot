28234b7f18c11afd9f11029d651cc6f5bbe5f0f9
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-01-17 02:22:23 +0900

    remove .test

--- crsbot_source/__init__.py
+++ crsbot_source/__init__.py
@@ -3,6 +3,5 @@ import os, glob
 from .random_select import *
 from .log import *
 from .get_json import *
-from .test import *
 
 __all__ = [os.path.split(os.path.splitext(file)[0])[1] for file in glob.glob(os.path.join(os.path.dirname(__file__), '[a-zA-Z0-9]*.py'))]
