52edd77149fc94e24e9bfa24adaf4f04417c3463
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-01-16 22:26:59 +0900

    Delete test.py

--- crsbot_source/test.py
+++
@@ -1,8 +0,0 @@
-from . import get_json
-#from .log import logger
-
-def test():
-    get_json.save_and_return_json("https://chunithm.sega.jp/data/common.json", "domestic")
-
-if __name__ == "__main__":
-    test()
