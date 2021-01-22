58f69b6ee9c3a06461d942d307cb3421cfb0cb1a
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-01-22 17:31:26 +0900

    正規表現改善

--- crsbot_source/__init__.py
+++ crsbot_source/__init__.py
@@ -6,4 +6,4 @@ from .get_json import *
 from .client import *
 from .exceptions import *
 
-__all__ = [os.path.split(os.path.splitext(file)[0])[1] for file in glob.glob(os.path.join(os.path.dirname(__file__), '[a-zA-Z0-9]*.py'))]
+__all__ = [os.path.split(os.path.splitext(file)[0])[1] for file in glob.glob(os.path.join(os.path.dirname(__file__), '[a-zA-Z0-9_]+.py'))]
