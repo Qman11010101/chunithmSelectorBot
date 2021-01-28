90cb3b1295881f4232f3fca9bdac2e197f25722b
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-01-29 01:09:43 +0900

    append params to function search_chunirec

--- crsbot_source/search.py
+++ crsbot_source/search.py
@@ -3,7 +3,7 @@ import json
 from .log import logger
 from .get_json import chunirec
 
-def search_chunirec():
+def search_chunirec(difficulty=None, difficulty_range=None, category=None, artist=None, notes=None, notes_range=None):
     pass
 
 # chunirec使えない時用
