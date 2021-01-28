cbb87ecb3173298c744b1a606e6a34fcb92cff05
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-01-28 22:31:49 +0900

    search.pyの追加

--- crsbot_source/__init__.py
+++ crsbot_source/__init__.py
@@ -1,9 +1,10 @@
 import os, glob
 
-from .random_select import *
 from .log import *
-from .get_json import *
 from .client import *
+from .get_json import *
+from .random_select import *
+from .search import *
 from .exceptions import *
 
 __all__ = [os.path.split(os.path.splitext(file)[0])[1] for file in glob.glob(os.path.join(os.path.dirname(__file__), '[a-zA-Z0-9_]+.py'))]
---
+++ crsbot_source/search.py
@@ -0,0 +1,11 @@
+import json
+
+from .log import logger
+from .get_json import chunirec
+
+def search_chunirec():
+    pass
+
+# chunirec使えない時用
+def search_official():
+    pass
