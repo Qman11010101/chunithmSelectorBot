86adc9ea88f455586ace5cf39166e1d29b75a30c
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-01-30 22:39:46 +0900

    定数ファイル作成

---
+++ crsbot_source/consts.py
@@ -0,0 +1,26 @@
+import json
+
+with open("setting.json", "r", encoding="UTF-8_sig") as s:
+    setting = json.load(s)
+
+# URL
+URL_International = "https://chunithm.sega.com/js/music/json/common.json"
+URL_Domestic = "https://chunithm.sega.jp/data/common.json"
+URL_chunirec = "https://api.chunirec.net/1.3/music/showall.json"
+
+# トークン
+CHUNIREC_TOKEN = setting["token"]["chunirec"]
+DISCORD_TOKEN = setting["token"]["discord"]
+
+# API関係
+API_LIFETIME = int(setting["misc"]["api_lifetime"])
+
+# logger関係
+tz = setting["misc"]["timezone"]
+is_logging = setting["logging"]["logging"]
+loglevel_stdio = setting["logging"]["loglevel_stdio"]
+loglevel_file = setting["logging"]["loglevel_file"]
+log_filename = setting["logging"]["log_filename"]
+
+# その他
+MAX_MUSICS = setting["misc"]["max_musics"]
