44f250d2b1cb4e4da98080b47c9d27de1f412691
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-04-03 02:00:02 +0900

    Update client.py

--- crsbot_source/client.py
+++ crsbot_source/client.py
@@ -1,15 +1,32 @@
-import discord
-from discord.ext import commands
+# import discord
+# from discord.ext import commands
 
-from .log import logger
-from .random_select import random_select, random_select_international
+# from .log import logger
+# from .random_select import random_select, random_select_international
+
+
+def command_parser(command):
+    cl = command.split()
+    for e in range(len(cl)):
+        if cl[e].lower() == "none":
+            cl[e] = "-"
+        elif ":" in cl[e]:
+            e_temp = cl[e].split(":")
+            cl[e] = [e_temp[0], e_temp[1]]
+    return cl
 
 
 class ChunithmSelector(commands.Cog):
-    pass
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def random(self, ctx):
+        pass
+
 
 class maimaiSelector(commands.Cog):
     pass
+
 
 class OngekiSelector(commands.Cog):
     pass
