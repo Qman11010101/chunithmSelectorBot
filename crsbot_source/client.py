61e290848ea2d10af92fbacdef200cd76e9cfbe5
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-04-03 03:06:07 +0900

    フォーマットなど

--- crsbot_source/client.py
+++ crsbot_source/client.py
@@ -1,8 +1,8 @@
-# import discord
-# from discord.ext import commands
+import discord
+from discord.ext import commands
 
-# from .log import logger
-# from .random_select import random_select, random_select_international
+from .log import logger
+from .random_select import random_select, random_select_international
 
 
 def command_parser(command):
@@ -15,18 +15,20 @@ def command_parser(command):
             cl[e] = [e_temp[0], e_temp[1]]
     return cl
 
-
 class ChunithmSelector(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def random(self, ctx):
-        pass
-
+    @commands.command(aliases=["rand", "r"])
+    async def random(self, ctx, *, arg=""):
+        if not arg: # 空ならエラー
+            err_mes = discord.Embed(title="Error", description="パラメータが入力されていません。", color=0xff0000)
+            await ctx.send(embed=err_mes)
+            return
+        await ctx.send(arg)
 
 class maimaiSelector(commands.Cog):
     pass
-
 
 class OngekiSelector(commands.Cog):
     pass
